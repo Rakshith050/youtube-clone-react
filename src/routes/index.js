ec7e0727979f29337360fba790ad577619a0aa38
Rakshith050 <rakshith14022001@gmail.com>
2024-01-27 11:14:58 +0530

    created sidebar & searchbar

---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+export { default as Feed } from "../components/Feed";
+export { default as VideoDetail } from "../components/VideoDetail";
+export { default as ChannelDetail } from "../components/ChannelDetail";
+export { default as SearchFeed } from "../components/SearchFeed";
+export { default as SearchBar} from "../components/SearchBar";
+export { default as Sidebar} from "../components/Sidebar";
